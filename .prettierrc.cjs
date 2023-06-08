a301995b2263c391b9afc82461a8e773dbf12ebc
imoo-mac <2779468693@qq.com>
2023-06-08 11:45:24 +0800

    feat: nice demo

---
+++ .prettierrc.cjs
@@ -0,0 +1,7 @@
+// .prettierrc.js
+module.exports = {
+  printWidth: 100, //一行的字符数，如果超过会进行换行，默认为80
+  singleQuote: true, // 字符串是否使用单引号，默认为 false，使用双引号
+  semi: false, // 行尾是否使用分号，默认为true
+  trailingComma: 'none' // 是否使用尾逗号
+}
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from 'react'
+import './App.css'
+import './demo'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return <>test</>
+}
+
+export default App
