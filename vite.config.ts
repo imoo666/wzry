e01552392414ca3fd5b3ed37fcb9c4eb65e8104a
imoo <859416516@qq.com>
2023-11-27 10:40:20 +0800

    fix: 去除单文件打包

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { viteSingleFile } from 'vite-plugin-singlefile'
+// import { viteSingleFile } from 'vite-plugin-singlefile'
 import viteCompression from 'vite-plugin-compression'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
-    viteSingleFile(),
+    // viteSingleFile(),
     viteCompression({
       threshold: 1024000 // 对大于 1mb 的文件进行压缩
     })
